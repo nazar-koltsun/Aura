5a2c8e7ffc5c1d9316d5d84287b5f0741681b28a
Nazar Koltsun <nazar-koltsun@ukr.net>
2025-01-23 16:45:53 +0200

    Main landing - About section is done

--- src/landings/MainLanding.jsx
+++ src/landings/MainLanding.jsx
@@ -12,6 +12,8 @@ import SeparatorBottomSimpleImage from '../assets/images/landing/separator-botto
 import SeparatorTopSimpleImage from '../assets/images/landing/separator-top-simple.svg';
 import HowToBg from '../assets/images/landing/how-to-bg.webp';
 import HowToBgMobile from '../assets/images/landing/how-to-bg-mobile.webp';
+import WhatAreNowAuraImage from '../assets/images/landing/about-aura.webp';
+import WhatAreNowAuraMobileImage from '../assets/images/landing/about-aura-mobile.webp'
 
 import PlayVideoIcon from '../components/icons/PlayVideoIcon';
 
@@ -95,29 +97,121 @@ const MainLanding = () => {
 
       {/* How to Section */}
       <section className="relative p-[70px] px-[70px] max-1024:px-4 max-1024:p-[50px] max-700:py-[40px]">
-        <div className='relative z-10 flex flex-col items-center'>
+        <div className="relative z-10 flex flex-col items-center">
           <Subtitle title="Jak to działa?" isShort />
 
-          <SectionTitle title="Jak zacząć rozmowę?" className="mt-3 max-1024:mt-8" />
+          <SectionTitle
+            title="Jak zacząć rozmowę?"
+            className="mt-3 max-1024:mt-8"
+          />
 
           <HowToSteps />
 
-          <div className='mt-14 flex justify-center gap-6 max-700:mt-6 max-700:flex-col max-700:gap-2.5 max-700:items-center'>
-            <Button to='#' className='min-w-[247px] min-h-[60px]'>Rozpocznij naukę</Button>
-            <Button to='#' variant='bordered' className='min-w-[262px] min-h-[60px] bg-white text-[var(--eerie-black)] max-700:min-w-[247px]'>Zobacz wideo</Button>
+          <div className="mt-14 flex justify-center gap-6 max-700:mt-6 max-700:flex-col max-700:gap-2.5 max-700:items-center">
+            <Button to="#" className="min-w-[247px] min-h-[60px]">
+              Rozpocznij naukę
+            </Button>
+            <Button
+              to="#"
+              variant="bordered"
+              className="min-w-[262px] min-h-[60px] bg-white text-[var(--eerie-black)] max-700:min-w-[247px]"
+            >
+              Zobacz wideo
+            </Button>
           </div>
         </div>
 
         <picture>
-          <source
-            srcSet={HowToBgMobile}
-            media="(max-width: 700px)"
+          <source srcSet={HowToBgMobile} media="(max-width: 700px)" />
+          <img
+            src={HowToBg}
+            alt=""
+            role="presentation"
+            className="absolute top-0 left-0 w-full h-full object-cover"
           />
-          <img src={HowToBg} alt="" role='presentation' className='absolute top-0 left-0 w-full h-full object-cover' />
         </picture>
 
-        <img src={SeparatorTopSimpleImage} alt="" role='presentation' className='absolute top-0 left-0 w-full' />
-        <img src={SeparatorBottomSimpleImage} alt="" role='presentation' className='absolute left-0 w-full bottom-[-1px] max-1920:bottom-[-1vw]' />
+        <img
+          src={SeparatorTopSimpleImage}
+          alt=""
+          role="presentation"
+          className="absolute top-0 left-0 w-full"
+        />
+        <img
+          src={SeparatorBottomSimpleImage}
+          alt=""
+          role="presentation"
+          className="absolute left-0 w-full bottom-[-1px] max-1920:bottom-[-1vw]"
+        />
+      </section>
+
+      {/* What are nowAura section */}
+      <section className="pl-20 pr-24 py-28 flex justify-center items-center gap-[131px] max-1240:gap-[80px] max-1024:px-6 max-1024:py-12 max-960:flex-col max-960:gap-9 max-700:py-10">
+        <div>
+          <picture>
+            <source
+              srcSet={WhatAreNowAuraMobileImage}
+              media="(max-width: 700px)"
+            />
+            <img
+              src={WhatAreNowAuraImage}
+              width={1120}
+              height={563}
+              alt=""
+              className="max-w-[620px] w-full"
+            />
+          </picture>
+        </div>
+        <div className="max-w-[500px] w-[55%] max-960:w-full">
+          <h2 className="text-[var(--eerie-black)] font-bold text-4xl leading-[50px] max-1024:text-[28px] max-1024:leading-[34px] max-960:text-center">
+            Czym są szkolenia nowAura?
+          </h2>
+
+          <div className="flex flex-col">
+            <p className="hidden max-960:block max-700:pl-4 mt-8 leading-[27px] text-[var(--granite-gray)]">
+              System oferuje scenariusze treningowe zaprojektowane specjalnie
+              dla trzech grup:
+            </p>
+            <p className="mt-8 leading-[27px] text-[var(--granite-gray)] max-960:hidden">
+              Nowoczesna platforma szkoleniowa oparta na sztucznej inteligencji,
+              która pozwala użytkownikom rozwijać umiejętności komunikacji w
+              formie interaktywnych symulacji rozmów. System oferuje scenariusze
+              treningowe zaprojektowane specjalnie dla trzech grup:
+            </p>
+
+            <ol className="mt-7 space-y-3 list-decimal max-1024:mt-5 max-700:pl-4">
+              <li className="text-lg font-bold leading-[30px] text-[var(--granite-gray)] max-1024:text-base">
+                Sprzedawców{' '}
+                <span className="font-normal">
+                  – ćwiczących rozmowy sprzedażowe z klientem, od nawiązywania
+                  relacji po rozwiązywanie obiekcji
+                </span>
+              </li>
+              <li className="text-lg font-bold leading-[30px] text-[var(--granite-gray)] max-1024:text-base">
+                Menedżerów{' '}
+                <span className="font-normal">
+                  – symulujących trudne rozmowy z pracownikiem, takie jak
+                  udzielanie informacji zwrotnej czy rozwiązywanie konfliktów
+                </span>
+              </li>
+              <li className="text-lg font-bold leading-[30px] text-[var(--granite-gray)] max-1024:text-base">
+                Przygotowujących się do rozmowy o pracę{' '}
+                <span className="font-normal">
+                  – ćwiczących rozmowę kwalifikacyjną z wirtualnym rekruterem
+                </span>
+              </li>
+            </ol>
+            <p className="hidden max-960:block max-700:pl-4 mt-8 leading-[27px] text-[var(--granite-gray)]">
+              Nowoczesna platforma szkoleniowa oparta na sztucznej inteligencji,
+              która pozwala użytkownikom rozwijać umiejętności komunikacji w
+              formie interaktywnych symulacji rozmów.
+            </p>
+          </div>
+
+          <Button to="#" className="mt-11 max-960:mx-auto">
+            Dowiedz się więcej
+          </Button>
+        </div>
       </section>
     </>
   );
