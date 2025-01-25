0a31d645c477ec13043924e3e2aefab50f6a8dc2
Nazar Koltsun <nazar-koltsun@ukr.net>
2025-01-25 11:52:41 +0200

    Landing main - added animation to HowToSteps component

--- src/components/landing/main/HowToSteps.jsx
+++ src/components/landing/main/HowToSteps.jsx
@@ -1,14 +1,37 @@
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { cn } from "../../../lib/utils";
+
 const steps = [
-  '1. Określ obszar do rozwoju',
-  '2. Wybierz temat rozmowy',
-  '3. Rozmawiaj swobodnie',
-  '4. Otrzymaj szczegółowy raport z pozytywnym i konstruktywnym feedbackiem',
+  "1. Określ obszar do rozwoju",
+  "2. Wybierz temat rozmowy",
+  "3. Rozmawiaj swobodnie",
+  "4. Otrzymaj szczegółowy raport z pozytywnym i konstruktywnym feedbackiem",
 ];
 
 const HowToSteps = () => {
+  const { ref, inView } = useInView({ threshold: 0.6 }); // Detect when the component is in view
+  const [activeIndex, setActiveIndex] = useState(null); // Track the currently active item
+
+  useEffect(() => {
+    if (inView) {
+      steps.forEach((_, index) => {
+        setTimeout(() => {
+          setActiveIndex(index);
+        }, index * 2000); // Stagger each item's activation by 500ms
+      });
+    } else {
+      setActiveIndex(null); // Reset when the component goes out of view
+    }
+  }, [inView]);
+
   return (
-    <div className="max-w-[533px] mt-9 bg-[var(--cultured)] p-7 pl-[75px] rounded-[30px] shadow-blockShadow max-700:pt-12
-      max-700:pb-12 max-700:px-7">
+    <div
+      ref={ref}
+      className="max-w-[533px] mt-9 bg-[var(--cultured)] p-7 pl-[75px] rounded-[30px] shadow-blockShadow max-700:pt-12
+      max-700:pb-12 max-700:px-7"
+    >
       <ol
         className="relative space-y-[98px] 
       before:content-['']
@@ -23,18 +46,39 @@ const HowToSteps = () => {
       "
       >
         {steps.map((step, index) => (
-          <li
+          <motion.li
             key={index}
-            className="relative flex items-center gap-[31px] text-[20px] leading-[25px] text-[var(--eerie-black)] font-medium max-700:text-center max-700:flex-col-reverse max-700:gap-5"
+            className="relative flex items-center gap-[31px] text-[20px] leading-[25px] font-medium max-700:text-center max-700:flex-col-reverse max-700:gap-5"
+            animate={{
+              color:
+                index === activeIndex ? "var(--jungle-green)" : "var(--eerie-black)",
+            }}
+            transition={{
+              color: { duration: 0.5, ease: "easeInOut" },
+            }}
           >
             <div
-              className="flex shrink-0 justify-center items-center w-7 h-7 rounded-full bg-[var(--light-silver)]"
+              className={cn('flex shrink-0 justify-center items-center w-7 h-7 rounded-full', 
+                index === activeIndex
+                ? "bg-[var(--jungle-green)]"
+                : "bg-[var(--light-silver)]")}
               role="presentation"
             >
-              <div className="w-3 h-3 rounded-full bg-[var(--jungle-green)] "></div>
+              <motion.div
+                className={cn('w-3 h-3 rounded-full', index === activeIndex ? "bg-[var(--cultured)]" : "bg-[var(--jungle-green)]")}
+                animate={{
+                  backgroundColor:
+                    index === activeIndex
+                      ? "var(--cultured)"
+                      : "var(--jungle-green)",
+                }}
+                transition={{
+                  backgroundColor: { duration: 0.5, ease: "easeInOut" },
+                }}
+              ></motion.div>
             </div>
             {step}
-          </li>
+          </motion.li>
         ))}
       </ol>
     </div>
