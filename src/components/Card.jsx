a2492692c71daa099b586f5191be5b91e68fdf25
Nazar Koltsun <nazar-koltsun@ukr.net>
2025-01-29 12:17:25 +0200

    Landing main - widjet improvments

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 import { cn } from '../lib/utils';
 
-const Card = ({ className, children }) => {
+const Card = ({ className, onClick, children }) => {
   return (
-    <div className={cn('bg-white py-9 px-8 shadow-blockShadow rounded-xl max-1024:py-5 max-1024:px-4', className)}>
+    <div className={cn('bg-white py-9 px-8 shadow-blockShadow rounded-xl max-1024:py-5 max-1024:px-4', className)} onClick={onClick}>
       {children}
     </div>
   );
--- src/components/landing/VideoWidget.jsx
+++ src/components/landing/VideoWidget.jsx
@@ -1,29 +1,36 @@
 import { useState } from 'react';
+import { cn } from '../../lib/utils';
+
 import Button from '../Button';
 import Modal from '../Modal';
 
 import TriangeIcon from '../icons/TriangeIcon';
+import { div } from 'framer-motion/client';
 
 const VideoWidget = ({ videoUrl }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isModalClicked, setIsModalClicked] = useState(false);
 
   const openModal = () => {
     setIsOpen(true);
-    document.body.style.overflow = 'hidden';
   };
 
   const closeModal = () => {
+    setTimeout(() => setIsModalClicked(false), 100);
     setIsOpen(false);
-    document.body.style.overflow = 'unset';
+  };
+
+  const onModalClick = () => {
+    setIsModalClicked(true);
   };
 
   let content = (
     <Button
       onClick={openModal}
-      className="fixed top-1/2 right-4 transform -translate-y-1/2 z-50 shadow-smallShadow max-1024:p-3"
+      className="fixed top-2/3 right-4 transform z-50 shadow-smallShadow max-1024:p-3 max-1024:text-xs max-1024:gap-1.5"
     >
-      <TriangeIcon className="rotate-90" />
-      <span className="max-1024:hidden">Zobacz video</span>
+      <TriangeIcon className="rotate-90 max-1024:w-3 max-1024:h-3" />
+      <span>Zobacz video</span>
     </Button>
   );
 
@@ -32,14 +39,20 @@ const VideoWidget = ({ videoUrl }) => {
       <Modal
         isOpen={isOpen}
         onClose={closeModal}
-        className="w-[800px] h-[452px] max-960:w-full max-960:h-full max-960:rounded-none max-520:pb-0 max-520:px-0 max-520:border-0"
+        onClick={onModalClick}
+        showBackdrop={isModalClicked ? true : false}
+        className={cn(
+          'w-[324px] h-[180px] top-2/3 right-4 left-auto transform-none translate-x-0 translate-y-0 p-0 border-0 max-1024:p-0',
+          isModalClicked &&
+            'w-[800px] h-[480px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 pt-8 max-960:w-full max-960:h-full max-960:rounded-none max-520:pb-0 max-520:px-0 max-520:border-0'
+        )}
       >
         <iframe
-          src={videoUrl}
+          className={cn('pointer-events-none', isModalClicked && 'pointer-events-auto')}
+          src={isModalClicked ? videoUrl + '&autoplay=1' : videoUrl}
           title="YouTube video player"
           frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          referrerPolicy="strict-origin-when-cross-origin"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
       </Modal>
