92e3dde76742199319bcb1fec2d8fefd55c9658f
Nazar Koltsun <nazar-koltsun@ukr.net>
2025-01-30 10:36:28 +0200

    Heade component added bigger z-index

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import UserIcon from './icons/UserIcon';
 
 const Header = ({ navItems = [], className }) => {
   return (
-    <header className={cn("col-span-2 flex justify-between items-center gap-6 px-14 py-2.5 border-b border-[var(--bright-gray)] max-1024:px-6 max-1024:gap-4 bg-white z-20", className)}>
+    <header className={cn("relative col-span-2 flex justify-between items-center gap-6 px-14 py-2.5 border-b border-[var(--bright-gray)] max-1024:px-6 max-1024:gap-4 bg-white z-30", className)}>
       <img className='max-1024:w-[180px] max-1024:h-auto"' src={Logo} width={216} height={110} alt="nowAura logo" />
 
       { navItems.length > 0 && <HeaderNav navItems={navItems} /> }
