360f6c2fe9a7be44d9771bd17cfefd6df0f342d2
Nazar Koltsun <nazar-koltsun@ukr.net>
2025-02-03 16:09:08 +0200

    Corporate landing - main section

--- src/components/landing/corporate/sections/MainSection.jsx
+++ src/components/landing/corporate/sections/MainSection.jsx
@@ -1,11 +1,15 @@
 import PageH1 from '../../PageH1';
 import Button from '../../../Button';
 
+import MainImage from '../../../../assets/images/landing/corporate-main-bg.webp';
+import MainImageMob from '../../../../assets/images/landing/corporate-main-bg-mob.webp';
+import SeparatorBottomSimpleImage from '../../../../assets/images/landing/separator-bottom-simple.svg';
+
 const MainSection = () => {
   return (
-    <section className="px-10 pt-20 pb-24">
-      <div className='max-w-[740px] ml-auto flex flex-col justify-center items-center'>
-        <PageH1>
+    <section className="relative px-10 pt-24 pb-36 max-1024:pt-14 max-1024:pb-[75px] max-1024:px-4">
+      <div className='relative z-10 max-w-[50vw] ml-auto flex flex-col justify-center items-center max-700:max-w-full max-700:ml-0'>
+        <PageH1 className='max-520:text-white'>
           <p>Platforma</p>
           <p className="inline-block bg-gradient-to-r from-[var(--sandy-brown)] from-0%  via-[var(--jungle-green)] via-54% to-[var(--celadon-green)] to-90% bg-clip-text text-transparent">
             kompetencji
@@ -13,7 +17,7 @@ const MainSection = () => {
           <p>miękkich dla firm.</p>
         </PageH1>
 
-        <p className="mt-11 text-[18px] leading-[30px] text-center text-[var(--granite-gray)] font-medium">
+        <p className="mt-11 text-[18px] leading-[30px] text-center text-[var(--granite-gray)] font-medium max-1024:mt-6 max-520:text-white">
           Odkryj nowy wymiar szkoleń interpersonalnych, które zbudują realną&nbsp;przewagę&nbsp;rynkową Twojej firmy.
         </p>
 
@@ -24,6 +28,18 @@ const MainSection = () => {
           Rozpocznij naukę
         </Button>
       </div>
+
+      <picture>
+        <source srcSet={MainImageMob} media="(max-width: 520px)" />
+        <img src={MainImage} className='absolute top-0 left-0 w-full h-full object-cover max-520:object-bottom' alt="" />
+      </picture>
+
+      <img
+        className="absolute bottom-[-1px] left-0 w-full max-1920:bottom-[-1vw]'"
+        src={SeparatorBottomSimpleImage}
+        alt=""
+        role="presentation"
+      />
     </section>
   );
 };
