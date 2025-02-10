f52c2417e4a5586b213960158e4250e7e335090b
Nazar Koltsun <nazar-koltsun@ukr.net>
2025-02-10 21:49:07 +0200

    Main landing - fixed animations on mobile

--- src/components/landing/main/HowToSteps.jsx
+++ src/components/landing/main/HowToSteps.jsx
@@ -1,6 +1,6 @@
+import { cn } from '../../../lib/utils';
 import { motion } from 'framer-motion';
 import { useEffect, useRef } from 'react';
-
 
 const steps = [
   '1. Określ obszar do rozwoju',
@@ -92,26 +92,28 @@ const HowToSteps = ({ isInView, activeIndex, setActiveIndex, isDesktop }) => {
     >
       <div className="relative">
         {/* Moving dot tracker */}
-        <motion.div
-          ref={dotRef}
-          className="absolute z-30 flex justify-center items-center left-0 top-0 w-7 h-7 bg-[var(--jungle-green)] rounded-full"
-          animate={{
-            top: `${activeIndex * 84}px`,
-          }}
-          initial={{ top: 0 }}
-          transition={{
-            duration: 0.5,
-            ease: 'easeInOut',
-          }}
-        >
-          <span className="absolute w-3 h-3 bg-white rounded-full"></span>
-        </motion.div>
+        {isDesktop && (
+          <motion.div
+            ref={dotRef}
+            className="absolute z-30 flex justify-center items-center left-0 top-0 w-7 h-7 bg-[var(--jungle-green)] rounded-full"
+            animate={{
+              top: `${activeIndex * 84}px`,
+            }}
+            initial={{ top: 0 }}
+            transition={{
+              duration: 0.5,
+              ease: 'easeInOut',
+            }}
+          >
+            <span className="absolute w-3 h-3 bg-white rounded-full"></span>
+          </motion.div>
+        )}
 
         <ol className="relative space-y-14 before:w-1.5 before:h-[275px] before:bg-[var(--light-silver)] before:absolute before:top-0 before:left-[11px] max-700:before:hidden max-700:space-y-5">
           {steps.map((step, index) => (
             <motion.li
               key={index}
-              className="relative flex items-start gap-[31px] text-[20px] leading-[25px] font-medium max-700:text-center max-700:flex-col-reverse max-700:gap-5"
+              className="relative flex items-start gap-[31px] text-[20px] leading-[25px] font-medium max-700:text-center max-700:flex-col-reverse max-700:gap-5 max-700:items-center"
               animate={{
                 color:
                   index === activeIndex
@@ -123,12 +125,27 @@ const HowToSteps = ({ isInView, activeIndex, setActiveIndex, isDesktop }) => {
               }}
             >
               <div
-                className='flex shrink-0 justify-center items-center w-7 h-7 rounded-full bg-[var(--light-silver)]'
+                className={cn(
+                  'flex shrink-0 justify-center items-center w-7 h-7 rounded-full bg-[var(--light-silver)]',
+                  index === activeIndex && 'max-1024:bg-[var(--jungle-green)]'
+                )}
                 role="presentation"
               >
-                <div
-                  className='w-3 h-3 rounded-full bg-[var(--jungle-green)]'
-                ></div>
+                <motion.div
+                  className={cn(
+                    'w-3 h-3 rounded-full bg-[var(--cultured)]',
+                    index === activeIndex && 'max-1024:bg-[var(--jungle-green)]'
+                  )}
+                  animate={{
+                    backgroundColor:
+                      index === activeIndex
+                        ? 'var(--cultured)'
+                        : 'var(--jungle-green)',
+                  }}
+                  transition={{
+                    backgroundColor: { duration: 0.5, ease: 'easeInOut' },
+                  }}
+                ></motion.div>
               </div>
               {step}
             </motion.li>
